9d0d2b4602393a45712bb41115c27e1a844732e7
David Dahan <ddahan@hudi.fr>
2019-02-13 15:40:00 +0100

    using inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pockey';
+    keyboard1ClassName: string;
+    keyboard2ClassName: string;
+
+    constructor() {
+        this.keyboard1ClassName = 'keyboard1';
+        this.keyboard2ClassName = 'keyboard2';
+    }
+
 }
--- src/app/simple-keyboard/simple-keyboard.component.ts
+++ src/app/simple-keyboard/simple-keyboard.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import Keyboard                                 from 'simple-keyboard';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import Keyboard                                        from 'simple-keyboard';
 
 @Component({
-  selector: 'app-simple-keyboard',
-  encapsulation: ViewEncapsulation.None,
-  templateUrl: './simple-keyboard.component.html',
-  styleUrls: ['./simple-keyboard.component.css']
+    selector: 'app-simple-keyboard',
+    encapsulation: ViewEncapsulation.None,
+    templateUrl: './simple-keyboard.component.html',
+    styleUrls: ['./simple-keyboard.component.css']
 })
 export class SimpleKeyboardComponent implements OnInit {
-  keyboard: Keyboard;
+    keyboard: Keyboard;
 
-  ngOnInit() {
-    this.keyboard = new Keyboard({});
-  }
+    @Input() keyboardClassName;
+
+    ngOnInit() {
+        console.log(`The class name will be: ` + this.keyboardClassName);
+        this.keyboard = new Keyboard(`.${ this.keyboardClassName }`, {});
+    }
 
 }

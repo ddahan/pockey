09d300efd52a2b91ba2cc0e0ed00ba4ed42e1a76
David Dahan <ddahan@hudi.fr>
2019-02-13 16:06:31 +0100

    css bug fix

--- src/app/simple-keyboard/simple-keyboard.component.ts
+++ src/app/simple-keyboard/simple-keyboard.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
-import Keyboard                                        from 'simple-keyboard';
+import { AfterViewInit, Component, Input, ViewEncapsulation } from '@angular/core';
+import Keyboard                                               from 'simple-keyboard';
 
 @Component({
     selector: 'app-simple-keyboard',
@@ -13,6 +13,7 @@ export class SimpleKeyboardComponent implements AfterViewInit {
     @Input() keyboardClassName;
 
     ngAfterViewInit() {
-        this.keyboard = new Keyboard(`.${ this.keyboardClassName }`, {});
+        this.keyboard = new Keyboard(`.${ this.keyboardClassName }`,
+            {theme: 'simple-keyboard hg-theme-default hg-layout-default', });
     }
 }
